82c19d482bdf0a031781c1a8a539799ef6313e26
Carlos Daniel <carlos.santos@o2obots.com>
2023-01-17 21:55:49 -0300

    fix: fixed swagger auth route name

--- src/application/auth/auth.controller.ts
+++ src/application/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 
@@ -7,6 +8,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('auth/login')
   @UseGuards(LocalAuthGuard)
+  @ApiTags('auth')
   async login(@Body() body: { email: string; password: string }) {
     return await this.authService.login(body);
   }
