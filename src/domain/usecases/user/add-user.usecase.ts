9975d700b8ed77a613db8063076a733c42930600
Carlos Daniel <carlos.santos@o2obots.com>
2023-01-22 21:39:45 -0300

    fix: fixed user with same email already created

--- src/domain/usecases/user/add-user.usecase.ts
+++ src/domain/usecases/user/add-user.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { AddUserDto } from '@/presentation/user/user.dto';
 import { CryptoHelper } from '@Helper/crypto.helper';
@@ -12,6 +12,13 @@ export class AddUserUseCase {
   ) {}
 
   async execute(user: AddUserDto): Promise<User> {
+    const userAlreadyExists = await this.userRepository
+      .findByEmail(user.email)
+      .catch(() => false);
+    if (userAlreadyExists)
+      throw new BadRequestException(
+        `user with email ${user.email} already exists`,
+      );
     const userWithHashedPassword = {
       ...user,
       password: await this.cryptoHelper.hash(user.password),
